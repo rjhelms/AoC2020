29bca7009dfe29a315a2ae49061f06c6ccfba8e7
rjhelms <robhailman@gmail.com>
2020-12-02 10:46:11 -0500

    Update variable names in 2b
    min_count and max_count aren't accurate for this part

--- 2b.py
+++ 2b.py
@@ -1,14 +1,14 @@
 class Password:
-    min_count = None
-    max_count = None
+    first_pos = None
+    second_pos = None
     letter = None
     password = None
 
     def valid(self):
         occurance = 0
-        if self.password[self.min_count - 1] == self.letter:
+        if self.password[self.first_pos - 1] == self.letter:
             occurance += 1
-        if self.password[self.max_count - 1] == self.letter:
+        if self.password[self.second_pos - 1] == self.letter:
             occurance += 1
         if occurance == 1:
             return True
@@ -22,8 +22,8 @@ with open("2a.txt", "r") as f:
         new_password = Password()
         split_row = row.split(" ")
         lengths = split_row[0].split("-")
-        new_password.min_count = int(lengths[0])
-        new_password.max_count = int(lengths[1])
+        new_password.first_pos = int(lengths[0])
+        new_password.second_pos = int(lengths[1])
         new_password.letter = split_row[1][0]
         new_password.password = split_row[-1][:-1]
         passwords.append(new_password)
